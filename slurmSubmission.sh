9c8cee9b222f84c0b709310a6393d6fbf936acf5
Sean Johnsen <sean.b.johnsen@gmail.com>
2022-03-02 14:45:03 -0500

    load python module before calling snakemake in slurmSubmission.sh

--- slurmSubmission.sh
+++ slurmSubmission.sh
@@ -3,4 +3,6 @@ then
 				mkdir slurmOut
 fi
 
-snakemake --use-envmodules --snakefile Snakefile --cluster-config slurmConfig.json -R all --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -e slurmOut/slurm-%j.err -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 500
+module load python/3.5.1
+
+snakemake --use-envmodules --snakefile Snakefile --rerun-incomplete --cluster-config slurmConfig.json -R all --latency-wait 60 --cluster "sbatch -J {rule} -o slurmOut/slurm-%j.out -e slurmOut/slurm-%j.err -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem} -A {cluster.account}" --jobs 500
